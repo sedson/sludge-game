2c2f47befb508ca39cf0d0cec00935e093d493e9
Carsten Rodin <carsten.rodin@gmail.com>
2024-06-04 09:19:17 -0400

    paddler gets tired

--- scripts/kayak-test-scene.js
+++ scripts/kayak-test-scene.js
@@ -36,6 +36,12 @@ export function setup(gumInstance, assets) {
 
   // Parent the camera to the kayak.
   g.camera.setParent(kayak);
+
+  // Data related to paddling the kayak
+  kayak.paddler = {
+    fatigue: 0,
+    restNeeded: 2000, // ms of rest to recover from each stroke
+  };
 }
 
 // The tick function
@@ -49,40 +55,66 @@ export function degrees_to_radians(degrees) {
   return degrees * (Math.PI / 180)
 }
 
-export function forward_left() {
-  // -Z is forward.
-
-  angle = angle - 10;
-  kayak.velocity = make_vector(angle, -1).add(kayak.velocity);
-  kayak.rotate(0, degrees_to_radians(angle), 0);
-}
-export function forward_right() {
-  angle = angle + 10;
-  kayak.velocity = make_vector(angle, -1).add(kayak.velocity);
-  kayak.rotate(0, degrees_to_radians(angle), 0);
-}
-export function backward_left() {
-  angle = angle - 15;
-  kayak.velocity = make_vector(angle, .5).add(kayak.velocity);
-  kayak.rotate(0, degrees_to_radians(angle), 0);
-}
-export function backward_right() {
-  angle = angle + 15;
-  kayak.velocity = make_vector(angle, .5).add(kayak.velocity);
-  kayak.rotate(0, degrees_to_radians(angle), 0);
+// Handle the impulse to paddle, as directed by player's keypress
+// if the paddler is too tired, they must rest before continuing
+async function paddle(direction) {
+    return new Promise((resolve, reject) => {
+	// are you tired yet?
+	if (kayak.paddler.fatigue < 2) {
+	    // no? ok, paddle this stroke
+	    switch (direction) {
+	    case "forwardleft":
+                // -Z is forward.
+		angle = angle - 10;
+		kayak.velocity = make_vector(angle, -1).add(kayak.velocity);
+		kayak.rotate(0, degrees_to_radians(angle), 0);
+		break;
+	    case "forwardright":
+		angle = angle + 10;
+		kayak.velocity = make_vector(angle, -1).add(kayak.velocity);
+		kayak.rotate(0, degrees_to_radians(angle), 0);
+		break;
+	    case "backwardleft":
+		angle = angle - 15;
+		kayak.velocity = make_vector(angle, .5).add(kayak.velocity);
+		kayak.rotate(0, degrees_to_radians(angle), 0);
+		break;
+	    case "backwardright":
+		angle = angle + 15;
+		kayak.velocity = make_vector(angle, .5).add(kayak.velocity);
+		kayak.rotate(0, degrees_to_radians(angle), 0);
+		break;
+	    default:
+		return;
+	    }
+	    // increment the fatigue counter
+    	    kayak.paddler.fatigue += 1;
+	    // then require a certain amount of rest
+	    setTimeout(resolve, kayak.paddler.restNeeded);
+	} else {
+	    // if you *are* tired, reject the promise
+	    reject();
+	}
+    }).then(() => {
+	// when the paddler is all rested up, decrement the counter
+	kayak.paddler.fatigue -= 1;
+    }).catch(() => {
+	// if the paddler was too tired, maybe tell the player
+	console.log("too tired...");
+    })
 }
 
 window.addEventListener('keydown', e => {
   if (e.key === 'q') {
-    backward_left();
+    paddle('backwardleft');
   }
   if (e.key === 'w') {
-    forward_left();
+    paddle('forwardleft');
   }
   if (e.key === 'o') {
-    forward_right();
+    paddle('forwardright');
   }
   if (e.key === 'p') {
-    backward_right();
+    paddle('backwardright');
   }
 })
