31eb57929291956cf8e1232c9fbeadc199b6e94d
Elijah Kennedy <elijahclimbs@gmail.com>
2024-06-06 00:31:37 -0400

    rebase changes

--- scripts/beacons-scene.js
+++ scripts/beacons-scene.js
@@ -5,10 +5,11 @@ let g;
 
 export function setup(gumInstance, assets) {
   g = gumInstance;
-  const OSCILLATOR_COUNT = 9
+  const OSCILLATOR_COUNT = 14
   for (let i = 0; i < OSCILLATOR_COUNT; i++) {
     g.audioEngine.createSpookyOscillator(`osc-${i}`)
     let location = randomWaterPoint(g, 220, 60)
+    console.log(location)
     g.beacons[`osc-${i}`] = location
   }
 }
@@ -18,7 +19,9 @@ export function draw(delta) {
   for (let osc in g.audioEngine.spookyOscillators) {
     if (osc === "undefined") continue;
     let location = g.beacons[osc]
-    let soundDiffMag = generateSoundDiff(g, location, kayak, 1)
-    g.audioEngine.spookyOscillatorVolume(osc, 0.005 / (10 + soundDiffMag))
+    let locationDiff = g.vec3(kayak.x - location.x, 0, kayak.y - location.z)
+    let locationDiffMag = Math.abs(locationDiff.x) + Math.abs(locationDiff.z)
+    let soundDiffMag = 1 / (1 + locationDiffMag)
+    g.audioEngine.spookyOscillatorVolume(osc, soundDiffMag / 8)
   }
 }
--- scripts/scene-sounds.js
+++ scripts/scene-sounds.js
@@ -2,7 +2,7 @@ let cidada_location;
 let sigh_location;
 let whale_location;
 
-let splashiesVolume = .11
+let splashiesVolume = .01
 
 function randomWorldPoint(g) {
 	return g.vec3(Math.floor(Math.random() * 200), 0, Math.floor(Math.random() * 200));
@@ -22,17 +22,17 @@ export function setup(g) {
 }
 
 export function loops(g, kayak) {
-	// const cicadaDiff = g.vec3(kayak.x - cidada_location.x, 0, kayak.z - cidada_location.z)
-	//   let cicadaDiffMag = .005 / (1 + Math.abs(cicadaDiff.x)) + .5 / (1 + Math.abs(cicadaDiff.z))
-	// g.audioEngine.loopVolume('cicadas', cicadaDiffMag / 2);
+	const cicadaDiff = g.vec3(kayak.x - cidada_location.x, 0, kayak.z - cidada_location.z)
+	let cicadaDiffMag = .005 / (1 + Math.abs(cicadaDiff.x)) + .5 / (1 + Math.abs(cicadaDiff.z))
+	g.audioEngine.loopVolume('cicadas', cicadaDiffMag / 2);
 
-	// const sighDiff = g.vec3(kayak.x - sigh_location.x, 0, kayak.z - sigh_location.z)
-	//   let sighDiffMag = .005 / (1 + Math.abs(sighDiff.x)) + .5 / (1 + Math.abs(sighDiff.z))
-	//   g.audioEngine.loopVolume('sighs', sighDiffMag / 2);
+	const sighDiff = g.vec3(kayak.x - sigh_location.x, 0, kayak.z - sigh_location.z)
+	  let sighDiffMag = .005 / (1 + Math.abs(sighDiff.x)) + .5 / (1 + Math.abs(sighDiff.z))
+	  g.audioEngine.loopVolume('sighs', sighDiffMag);
 
-	// const whale_diff = g.vec3(kayak.x - whale_location.x, 0, kayak.z - whale_location.z)
-	//   let whaleDiffMag = .005 / (1 + Math.abs(whale_diff.x)) + .5 / (1 + Math.abs(whale_diff.z))
-	// g.audioEngine.loopVolume('whale', whaleDiffMag / 2);
+	const whale_diff = g.vec3(kayak.x - whale_location.x, 0, kayak.z - whale_location.z)
+	  let whaleDiffMag = .005 / (1 + Math.abs(whale_diff.x)) + .5 / (1 + Math.abs(whale_diff.z))
+	g.audioEngine.loopVolume('whale', whaleDiffMag);
 
 }
 
